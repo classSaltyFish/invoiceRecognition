1f4acfb06bd5f079e91f57bd8c89937a295a51f5
coldsun233 <2679618366@qq.com>
2020-02-27 16:01:55 +0800

    改进获取发票列表的代码

--- apps/invoice/views/list.py
+++ apps/invoice/views/list.py
@@ -6,22 +6,23 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.core.paginator import Paginator, InvalidPage
-import json
+
+from apps.invoice.serializers import MySerializer
 
 
 # 管理端视图
 # url:invoice/list
 class InvoiceList(APIView):
-    '''分页显示发票信息'''
+    """分页显示发票信息"""
     # # 测试用
     # permission_classes = (AllowAny,)
-    #
+
     # # 运行使用
-    # authentication_classes = (TokenAuthentication,)
-    # permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     def get(self, request):
-        '''
+        """
         GET请求
         :param request:
         key:   'all'  # 请求所有发票
@@ -30,19 +31,10 @@ class InvoiceList(APIView):
         current:  '1'  # 当前页码
         pageSize: '9'  # 每页显示条目数
         :return:
-        '''
+        """
         page_size = request.GET.get('pageSize')
         current_page = request.GET.get("current")
         key = request.GET.get('key')
-        originHeads = request.META.get("HTTP_ORIGIN")  # 获取请求的主机地址
-        headers = {
-            'Access-Control-Allow-Origin': originHeads,
-            'Access-Control-Allow-Credentials': True,
-            'Access-Control-Allow-Methods': 'POST, GET, PUT, OPTIONS, DELETE, PATCH',
-            'Access-Control-Max-Age': '3600',
-            'Access-Control-Allow-Headers': 'token,Origin, X-Requested-With, Content-Type, Accept,mid,X-Token'
-        }
-
         if key == 'wait':
             queryset = Invoice.objects.filter(status__in=[0])
         elif key == 'record':
@@ -51,40 +43,15 @@ class InvoiceList(APIView):
             queryset = Invoice.objects.all()
         total = queryset.count()
 
-        data = []
-        # 把url取出来,然后把发票数据取成json格式
-        for e in queryset:
-            temp = dict()
-            # 单张图片的用户的url的取法
-            image = e.image.first()  # 这里是避免有多张图片时，出现错误
-            if image is None:
-                temp['imgUrl'] = ''
-            else:
-                temp['imgUrl'] = image.img.url
-            temp['id'] = e.id
-            temp['invoiceCode'] = e.invoiceCode
-            temp['invoiceDate'] = e.invoiceDate
-            temp['invoiceNum'] = e.invoiceNum
-            temp['invoiceType'] = e.invoiceType
-            temp['commodityInfo'] = json.loads(e.commodityInfo)
-            temp['status'] = e.status
-            temp['processDate'] = e.processDate
-            temp['processor'] = e.processer
-            temp['purchaseInfo'] = json.loads(e.purchaserInfo)
-            temp['sellerInfo'] = json.loads(e.sellerInfo)
-            temp['uploader'] = e.uploader_id
-            temp['uploadDate'] = e.uploadDate
-            temp['invoiceMoney'] = e.invoiceMoney
-            data.append(temp)
-
         # 对分页进行处理，并且捕获异常和处理异常
         try:
-            p = Paginator(data, page_size)
-            contacts = p.page(current_page).object_list
+            p = Paginator(queryset, page_size)
+            contacts = p.page(current_page)
+            result = MySerializer(instance=contacts, many=True)
         except InvalidPage:
-            return Response({"msg": "页码有错误"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR, headers=headers)
+            return Response({"msg": "页码有错误"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         context = {
-            "results": contacts,
+            "results": result.data,
             "msg": True,
             "pagination": {
                 "total": total,
@@ -92,4 +59,4 @@ class InvoiceList(APIView):
                 "current": current_page
             }
         }
-        return Response(context, status=status.HTTP_200_OK, headers=headers)
+        return Response(context, status=status.HTTP_200_OK)
