fa20bc8d6c82d167189f5c80a2012b995e7c7005
黑里俏 <1012090981@qq.com>
2020-02-28 17:01:23 +0800

    '完善了用户管理的查询功能'

--- apps/user/views/list.py
+++ apps/user/views/list.py
@@ -20,29 +20,54 @@ class UserList(APIView):
     # permission_classes = (AllowAny,)
 
     # 运行使用
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
+    # authentication_classes = (TokenAuthentication,)
+    # permission_classes = (IsAuthenticated,)
 
-    def post(self, request):
+    def get(self, request):
         """
 
         :param request:排序方式 当前页数 和每页显示的数目
         :return:
         """
-        data = json.load(request)
-        pageSize = data['pageSize']
-        sorter = data['sorter']
-        current = data["current"]
+        pageSize = request.GET.get('pageSize')
+        sorter = request.GET.get('sorter')
+        current = request.GET.get('current')
+        id=request.GET.get('id')
+        nickname=request.GET.get('nickname')
+        user_status=request.GET.get('status')
 
-        # 如果排序方式为空或者无排序方式，就采用默认排序方式
-        if sorter is None or sorter == '':
-            queryset = User.objects.all().order_by("id")
-        elif str(sorter).endswith('descend'):
-            queryset = User.objects.all().order_by('-' + str(sorter).split("_")[0])
+        if id is None and nickname is None and user_status is None:
+            # 如果排序方式为空或者无排序方式，就采用默认排序方式
+            if sorter is None or sorter == '':
+                queryset = User.objects.all().order_by("id")
+            elif str(sorter).endswith('descend'):
+                queryset = User.objects.all().order_by('-' + str(sorter).split("_")[0])
+            else:
+                queryset = User.objects.all().order_by(str(sorter).split("_")[0])
+        #以下是分情况的查询
+        elif id and nickname and user_status:
+            queryset=User.objects.filter(id=id,nickname__contains=nickname,status=user_status)
+        elif id and nickname:
+            queryset = User.objects.filter(id=id, nickname__contains=nickname)
+        elif id and user_status:
+            queryset = User.objects.filter(id=id, status=user_status)
+        elif nickname and user_status:
+            queryset=User.objects.filter(nickname__contains=nickname,status=user_status)
+        elif id:
+            queryset = User.objects.filter(id=id)
+        elif nickname:
+            queryset = User.objects.filter(nickname__contains=nickname)
         else:
-            queryset = User.objects.all().order_by(str(sorter).split("_")[0])
-        total = queryset.count()
+            queryset = User.objects.filter(status=user_status)
+        if queryset is None:
+            return Response({"data": "",
+            "msg": True,
+            "total":0,
+            "pageSize": pageSize,
+            "current": current},status=status.HTTP_200_OK)
 
+
+        total = queryset.count()
         try:
             p = Paginator(queryset, pageSize)
             contacts = p.page(current).object_list
